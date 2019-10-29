e98b8fb920189df260d7038b200176b1e10c869d
schen225 <schen225@asu.edu>
2019-10-29 15:33:50 -0700

    Adding more html and js functions

--- functions.js
+++ functions.js
@@ -1,5 +1,10 @@
 function summarizeButton() {
     // Needs API call here
     // Making it so that when the button is clicked, the summary API is called + the rest of the page will show
-    
+
+}
+
+function spitToMedias() {
+    // When clicked, this button will enable user to approve the summary that was given to them,
+    // and put the summary out onto different media platforms (Slack, Twitter, etc.)
 }
